be62c62649534454794d614df6388998c0c217be
Alexey Solovyev <solovyev.alexey@gmail.com>
2024-04-23 21:38:16 -0600

    Read the HOL Light help directory and populate helpItems (init)

--- src/help.ts
+++ src/help.ts
@@ -1,4 +1,6 @@
 import * as vscode from 'vscode';
+import * as fs from 'fs/promises';
+import * as path from 'path';
 
 class HelpItem {
     name: string;
@@ -11,20 +13,68 @@ class HelpItem {
     }
 }
 
+/* eslint-disable @typescript-eslint/naming-convention */
+const SPECIAL_NAMES: {[key: string]: string} = {
+    '.joinparsers': '++',
+    '.orparser': '|||',
+    '.singlefun': '|=>',
+    '.upto': '--',
+    '.valmod': '|->',
+    'insert_prime': "insert'",
+    'mem_prime': "mem'",
+    'subtract_prime': "subtract'",
+    'union_prime': "union'",
+    'unions_prime': "unions'",
+    'ALPHA_UPPERCASE': 'ALPHA',
+    'CHOOSE_UPPERCASE': 'CHOOSE',
+    'CONJUNCTS_UPPERCASE': 'CONJUNCTS',
+    'EXISTS_UPPERCASE': 'EXISTS',
+    'HYP_UPPERCASE': 'HYP',
+    'INSTANTIATE_UPPERCASE': 'INSTANTIATE',
+    'INST_UPPERCASE': 'INST',
+    'MK_BINOP_UPPERCASE': 'MK_BINOP',
+    'MK_COMB_UPPERCASE': 'MK_COMB',
+    'MK_CONJ_UPPERCASE': 'MK_CONJ',
+    'MK_DISJ_UPPERCASE': 'MK_DISJ',
+    'MK_EXISTS_UPPERCASE': 'MK_EXISTS',
+    'MK_FORALL_UPPERCASE': 'MK_FORALL',
+    'REPEAT_UPPERCASE': 'REPEAT'
+};
+
 export class HelpProvider {
     private helpItems: HelpItem[] = [];
 
     // Loads (or reloads) all help items from the given path to HOL Light
     async loadHelpItems(holPath: string) {
-        console.log(`Loading from: ${holPath}`);
         if (!holPath) {
             return;
         }
-        this.helpItems = [];
-        // this.helpItems = [new HelpItem('ASM_REWRITE_TAC'), new HelpItem('ASM_SIMP_TAC')];
+        const helpPath = path.join(holPath, 'Help');
+        console.log(`Loading help items from: ${helpPath}`);
+        try {
+            const stat = await fs.stat(helpPath);
+            if (!stat.isDirectory()) {
+                console.error(`Not a directory: ${helpPath}`);
+                return;
+            }
+            const items = [];
+            for (const file of await fs.readdir(helpPath, {withFileTypes: true})) {
+                if (file.isFile() && file.name.endsWith('.hlp')) {
+                    let name = file.name.slice(0, -4);
+                    if (name in SPECIAL_NAMES) {
+                        name = SPECIAL_NAMES[name];
+                    }
+                    items.push(new HelpItem(name));
+                }
+            }
+            this.helpItems = items;
+        } catch(err) {
+            console.error(`Error: ${err}`);
+        }
     }
 
     provideCompletionItems(document: vscode.TextDocument, position: vscode.Position, token: vscode.CancellationToken, context: vscode.CompletionContext) {
+        // TODO: special symbols (e.g., ++, |||) are not words
         const range = document.getWordRangeAtPosition(position);
         if (!range || !this.helpItems.length) {
             return [];
