7e74e7166fe71699e4c0de0095c11b89b5b52bc8
Alexey Solovyev <solovyev.alexey@gmail.com>
2025-09-10 20:59:20 -0600

    Use webpack to compile the extension and support for React webviews

---
+++ src/goalview/App.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', padding: 20 }}>
+      <h1>Hello from React inside VS Code Webview 🚀</h1>
+    </div>
+  );
+}
