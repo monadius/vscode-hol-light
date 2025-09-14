cbac616b5299dcce171a7983762729a3155934d1
Alexey Solovyev <solovyev.alexey@gmail.com>
2025-09-14 17:45:45 -0600

    goalview: Add tailwind

--- goalview/vite.config.ts
+++ goalview/vite.config.ts
@@ -1,9 +1,10 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: "dist",
     rollupOptions: {
