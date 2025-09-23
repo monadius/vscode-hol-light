9a59db52ab77f8cd61183f101c9261ff7ec76c84
Alexey Solovyev <solovyev.alexey@gmail.com>
2025-09-22 20:14:55 -0600

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,37 +3,46 @@
 'use strict';
 
 const path = require('path');
-const webpack = require('webpack');
 
 //@ts-check
 /** @typedef {import('webpack').Configuration} WebpackConfig **/
 
 /** @type WebpackConfig */
-module.exports = (_env, argv) => {
-  const isProd = argv.mode === 'production';
-  
-  return [
-    // Extension (runs in Node)
-    {
-      mode: argv.mode || 'none',
-      target: 'node',
-      entry: {
-        extension: './src/extension.ts'
-      },
-      output: {
-        path: path.resolve(__dirname, 'out'),
-        filename: '[name].js',
-        libraryTarget: 'commonjs2'
-      },
-      resolve: {
-        extensions: ['.ts', '.js']
-      },
-      devtool: 'nosources-source-map',
-      module: {
-        rules: [{ test: /\.ts$/, use: 'ts-loader', exclude: /node_modules/ }]
-      },
-      externals: { vscode: 'commonjs vscode' }
-    },
+const extensionConfig = {
+  target: 'node', // VS Code extensions run in a Node.js-context 📖 -> https://webpack.js.org/configuration/node/
+	mode: 'none', // this leaves the source code as close as possible to the original (when packaging we set this to 'production')
 
-  ];
+  entry: './src/extension.ts', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
+  output: {
+    // the bundle is stored in the 'out' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
+    path: path.resolve(__dirname, 'out'),
+    filename: 'extension.js',
+    libraryTarget: 'commonjs2'
+  },
+  externals: {
+    vscode: 'commonjs vscode' // the vscode-module is created on-the-fly and must be excluded. Add other modules that cannot be webpack'ed, 📖 -> https://webpack.js.org/configuration/externals/
+    // modules added here also need to be added in the .vscodeignore file
+  },
+  resolve: {
+    // support reading TypeScript and JavaScript files, 📖 -> https://github.com/TypeStrong/ts-loader
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  devtool: 'nosources-source-map',
+  infrastructureLogging: {
+    level: "log", // enables logging required for problem matchers
+  },
 };
+module.exports = [ extensionConfig ];
